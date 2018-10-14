c90149ddcda8215eaed38ab0f7ff0a85bf52e105
Ricardo Fonseca <ric.bicas@gmail.com>
2018-10-14 12:48:07 +0100

    Adds beginning of Project2, and updates to the report of project1

---
+++ Project 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public const float MAX_SPEED = 5.0f;
+    private const float FALL_LIMIT = -12575.0f;
+
+    private bool walking = false;
+    private Vector3 spawnPoint;
+
+	// Use this for initialization
+	void Start () {
+        spawnPoint = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (walking)
+        {
+            transform.position += Camera.main.transform.forward * MAX_SPEED * Time.deltaTime;
+        }
+
+        if(transform.position.y < FALL_LIMIT)
+        {
+            transform.position = spawnPoint;
+        }
+
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        if(Physics.Raycast (ray, out hit))
+        {
+            if (hit.collider.name.Contains("Floor") )
+            {
+                walking = false;
+            }
+            else
+                walking = true;
+        }
+
+    }
+}
---
+++ Project 2/Assets/Scripts/TerrainController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainController : MonoBehaviour
+{
+
+    private Terrain terrain;
+
+    private Vector3 terrainSize;
+
+    public float sizeX;
+    public float sizeY;
+    public float sizeZ;
+
+    void Start()
+    {
+
+        terrain = GetComponent<Terrain>();
+        terrainSize = new Vector3(sizeY, sizeZ, sizeX);
+        terrain.terrainData.size = terrainSize;
+
+    }
+
+}
