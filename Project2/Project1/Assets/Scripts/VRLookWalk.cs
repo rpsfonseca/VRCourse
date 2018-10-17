2e93e0363f75fcaf5d43382d60c554be321a8540
yqbk <jksyrek@gmail.com>
2018-10-17 17:54:33 +0100

    Camera movement - part 1

---
+++ Project2/Project1/Assets/Scripts/VRLookWalk.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRLookWalk : MonoBehaviour
+{
+
+    public Transform vrCamera;
+
+    public float toggleAngle = 45.0f;
+
+    public float speed = 3.0f;
+
+    public bool moveforward;
+    public bool movebackward;
+
+    private CharacterController cc;
+
+    // Use this for initialization
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (vrCamera.eulerAngles.x >= toggleAngle )
+        {
+            moveforward = true;
+            movebackward = false;
+        } 
+        else if ( vrCamera.eulerAngles.x <= -toggleAngle) 
+        {
+            moveforward = false;
+            movebackward = true;
+        }
+
+        else
+        {
+            moveforward = false;
+            movebackward = false;
+        }
+
+        if (moveforward == true)
+        {
+            Vector3 forward = vrCamera.TransformDirection(Vector3.forward);
+
+            cc.SimpleMove(forward * speed);
+        }
+        
+        if (movebackward == true)
+        {
+            Vector3 backward = vrCamera.TransformDirection(-Vector3.forward);
+
+            cc.SimpleMove(backward * speed);
+        }
+    }
+}
