bc86b32942a15a700afe6bfd797140d548bfb3da
Ricardo Fonseca <ric.bicas@gmail.com>
2018-10-21 19:51:58 +0100

    Adds blazing action to dragon

---
+++ Project2/Project1/Assets/Scripts/AttackTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackTrigger : MonoBehaviour
+{
+    public Blaze blaze;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player" && blaze)
+        {
+            blaze.StartBlazing();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Player" && blaze)
+        {
+            blaze.StopBlazing();
+        }
+    }
+}
---
+++ Project2/Project1/Assets/Scripts/Blaze.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blaze : MonoBehaviour
+{
+    public List<ParticleSystem> particleSystems;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartBlazing()
+    {
+        for (int i = 0; i < particleSystems.Count; i++)
+        {
+            particleSystems[i].Play();
+        }
+    }
+
+    public void StopBlazing()
+    {
+        for (int i = 0; i < particleSystems.Count; i++)
+        {
+            particleSystems[i].Stop();
+        }
+    }
+}
