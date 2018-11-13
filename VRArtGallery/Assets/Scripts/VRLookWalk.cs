34ced37aafd5c2da314a44191530bfa63b1f945d
louro11 <goncalo_louro@hotmail.com>
2018-11-13 03:36:22 +0000

    add interaction scene

---
+++ VRArtGallery/Assets/Scripts/VRLookWalk.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRLookWalk : MonoBehaviour
+{
+
+    public Transform vrCamera;
+
+    public float toggleAngle = 30.0f;
+
+    public float speed = 3.0f;
+
+    public bool moveforward;
+    public bool movebackward;
+
+    private CharacterController cc;
+
+    // Use this for initialization
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (vrCamera.eulerAngles.x >= 180 && vrCamera.eulerAngles.x <= (360 - toggleAngle) )
+        {
+            moveforward = true;
+            movebackward = false;
+        } 
+        else if (vrCamera.eulerAngles.x <=180 && vrCamera.eulerAngles.x >= toggleAngle) 
+        {
+            moveforward = false;
+            movebackward = true;
+        }
+
+        else
+        {
+            moveforward = false;
+            movebackward = false;
+        }
+
+        if (moveforward == true)
+        {
+            Vector3 forward = vrCamera.TransformDirection(Vector3.forward);
+
+            Debug.Log("forward: " + forward);
+            Debug.Log("position: " + transform.position);
+
+            cc.SimpleMove(forward * speed);
+        }
+        
+        if (movebackward == true)
+        {
+            Vector3 backward = vrCamera.TransformDirection(-Vector3.forward);
+
+            cc.SimpleMove(backward * speed);
+        }
+    }
+}
---
+++ VRArtGallery/Assets/Scripts/VRStare_and_Grab.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRStare_and_Grab : MonoBehaviour {
+
+    public float stare_time = 0f; // timer 
+    public Transform VRHand;
+    public Rigidbody TargetObject;
+    public bool grabbed = false;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        stare_time = stare_time + Time.deltaTime;
+
+        if (stare_time >= 2f && VRHand.childCount == 0) // once a certain amount of time has passed, the object will be 'grabbed'
+        {
+            GrabObject();
+        }
+	}
+
+    public void ResetStareTime()
+    {
+        stare_time = 0f;
+    }
+
+    public void GrabObject()
+    {
+        grabbed = true;
+        TargetObject.transform.parent = VRHand.transform;
+        TargetObject.transform.position = VRHand.transform.position + (VRHand.transform.forward * 10.0f);
+        TargetObject.transform.eulerAngles = new Vector3(
+             VRHand.transform.eulerAngles.x + TargetObject.transform.eulerAngles.x + 60,
+             VRHand.transform.eulerAngles.y + TargetObject.transform.eulerAngles.y - 90,
+             VRHand.transform.eulerAngles.z + TargetObject.transform.eulerAngles.z 
+        );
+    }
+}
