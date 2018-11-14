5cdd89d7b73b61063017780e0fa81592a3913545
louro11 <goncalo_louro@hotmail.com>
2018-11-14 13:44:09 +0000

    add interaction marker

---
+++ VRArtGallery/Assets/Scripts/ReticleChanger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReticleChanger : MonoBehaviour {
+
+    public GameObject reticleIn;
+    public GameObject reticleOut;
+
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+
+        if(Physics.Raycast (transform.position, transform.TransformDirection(Vector3.forward), out hit))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward));
+            if (hit.transform.gameObject.tag.Equals("Art"))
+            {
+                Debug.Log("hit art");
+                reticleIn.GetComponent<Renderer>().enabled = true;
+                reticleOut.GetComponent<Renderer>().enabled = false;
+            }
+            else
+            {
+                Debug.Log("no hit art");
+                reticleIn.GetComponent<Renderer>().enabled = false;
+                reticleOut.GetComponent<Renderer>().enabled = true;
+            }
+        }
+
+	}
+}
