ce72a95638f2571ffa6e00f64346e9ef655a1ed4
louro11 <goncalo_louro@hotmail.com>
2018-11-29 05:02:45 +0000

    banksy animation progress

---
+++ VRArtGallery/Assets/ShredPaiting.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShredPaiting : MonoBehaviour {
+
+    public float shredSpeed = 0.7f;
+    public float shredRange = 0.5f;
+    public bool canShred = false;
+
+    public Transform parent;
+
+    private void Start()
+    {
+        parent = this.GetComponentInParent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            canShred = true;
+        }
+
+        if (canShred)
+        {
+            Shred();
+        }
+
+	}
+
+    public void Shred()
+    {
+        float py = transform.position.y;
+        transform.Translate(Vector3.down * shredSpeed * Time.deltaTime, Space.World);
+        parent.Translate(Vector3.down * shredSpeed * Time.deltaTime, Space.World);
+    }
+
+}
