c2df8bb5b8a9d7bbdc239ea7fc928fabda5cc2c1
Ricardo Fonseca <ric.bicas@gmail.com>
2018-12-08 02:16:30 +0000

    Now teleports by loading a new scene

---
+++ VRArtGallery/Assets/Scripts/CaravaggioAnimatedPainting.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CaravaggioAnimatedPainting : Interactable
+{
+    public bool playing = false;
+    public Light light;
+
+    public UnityEngine.Video.VideoPlayer videoPlayer;
+
+    public override void StartInteraction()
+    {
+        //PlayVideo();
+        videoPlayer.Stop();
+        //StartCoroutine("CallEnd");
+        InteractionManager.RemoveCurrentInteractable();
+        SceneManager.LoadScene(1);
+    }
+
+    public override void Interaction()
+    {
+    }
+
+    public void PlayVideo()
+    {
+        videoPlayer.Play();
+        playing = true;
+    }
+
+    public override void EndInteraction()
+    {
+    }
+
+    IEnumerator CallEnd()
+    {
+        yield return new WaitForSeconds(104);
+        InteractionManager.RemoveCurrentInteractable();
+    }
+}
