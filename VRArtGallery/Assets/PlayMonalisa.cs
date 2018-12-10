a38b92c9018d7584f23f03389d8ae47aa169425f
louro11 <goncalo_louro@hotmail.com>
2018-12-10 23:39:05 +0000

    monalisa

---
+++ VRArtGallery/Assets/PlayMonalisa.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMonalisa : MonoBehaviour {
+
+
+    public UnityEngine.Video.VideoPlayer videoPlayer;
+    public GameObject painting;
+
+    void Start () {
+
+        painting = this.GetComponentInChildren<GameObject>();
+        videoPlayer = this.GetComponent<UnityEngine.Video.VideoPlayer>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            
+            videoPlayer.enabled = true;
+            videoPlayer.Play();
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            videoPlayer.enabled = false;
+        }
+    }
+
+}
