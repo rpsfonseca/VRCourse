ea6ff34b6aa10112c4f79391a6b5217557f2a6f9
yqbk <jksyrek@gmail.com>
2018-12-11 21:15:59 +0000

    Add some painting descriptions

--- VRArtGallery/Assets/Scripts/InfoForArtPieces/ArtInfo.cs
+++ VRArtGallery/Assets/Scripts/InfoForArtPieces/ArtInfo.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Artwork", menuName = "Pieces")]
 public class ArtInfo : ScriptableObject
 {
+    public string title;
     public string painter;
     public string date;
     public string description;
