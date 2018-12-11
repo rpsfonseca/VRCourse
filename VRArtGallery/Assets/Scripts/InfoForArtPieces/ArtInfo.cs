e3b901950a841a58f8bffedb5e6ac5c17009dda5
Ricardo Fonseca <ric.bicas@gmail.com>
2018-12-11 20:29:22 +0000

    Adds scriptable object for art info

---
+++ VRArtGallery/Assets/Scripts/InfoForArtPieces/ArtInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Artwork", menuName = "Pieces")]
+public class ArtInfo : ScriptableObject
+{
+    public string painter;
+    public string date;
+    public string description;
+}
